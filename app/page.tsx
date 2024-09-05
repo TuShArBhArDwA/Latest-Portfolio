4649c6b90077867d8c53e06252e3b19d44c666d8
Tushar Bhardwaj <tusharbhardwaj2617@gmail.com>
2024-09-06 00:09:16 +0530

    set

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import {FaHome} from "react-icons/fa"
 import { navItems } from "@/data";
@@ -16,6 +18,8 @@ export default function Home() {
       <RecentProjects />
       <Clients />
       <Experience />
+      <Approach />
+      <Footer />
     </div>
    </main>
   );
--- data/index.ts
+++ data/index.ts
@@ -205,13 +205,16 @@ export const navItems = [
     {
       id: 1,
       img: "/git.svg",
+      link: "https://github.com/TuShArBhArDwA",
     },
     {
       id: 2,
       img: "/twit.svg",
+      link: "https://twitter.com/TusharBhard2004",
     },
     {
       id: 3,
       img: "/link.svg",
+      link: "https://www.linkedin.com/in/bhardwajtushar2004/",
     },
   ];
