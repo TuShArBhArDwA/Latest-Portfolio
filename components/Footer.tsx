5cd5703565f8812e53f4113b2dd96603fc17475f
Tushar Bhardwaj <tusharbhardwaj2617@gmail.com>
2024-09-06 01:06:59 +0530

    touch

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,16 +4,7 @@ import MagicButton from "./ui/MagicButton";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
-      {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50 "
-        />
-      </div>
-
+    <footer className="w-full pb-10 mb-[100px] md:mb-5" id="contact">
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">your</span> digital
@@ -38,12 +29,15 @@ const Footer = () => {
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-              <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
+             <a
+             key={info.id}
+             href={info.link} // Add the link here
+             target="_blank" // Optional: Open the link in a new tab
+             rel="noopener noreferrer" // Optional: Improve security when opening in a new tab
+             className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+           >
+             <img src={info.img} alt="icons" width={20} height={20} />
+           </a>
           ))}
         </div>
       </div>
