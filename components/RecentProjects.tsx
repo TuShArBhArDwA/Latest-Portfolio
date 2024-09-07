6e69f95e57a1fc62e37607fbe07d725e952e0228
Tushar Bhardwaj <tusharbhardwaj2617@gmail.com>
2024-09-07 21:52:24 +0530

    ok

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -13,9 +13,14 @@ const RecentProjects = () => {
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map((item) => (
           <div
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
             key={item.id}
+            // href={item.link}
+            // target="_blank"
+            rel="noopener noreferrer"
+            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw] no-underline"
           >
+
+            <a href={item.link} target="_blank">
             <PinContainer
               title="github.com/TuShArBhArDwA"
               href="https://topmate.io/tusharbhardwaj"
@@ -71,6 +76,7 @@ const RecentProjects = () => {
                 </div>
               </div>
             </PinContainer>
+            </a>
           </div>
         ))}
       </div>
