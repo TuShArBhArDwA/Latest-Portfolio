9cddc75162f9f5ecd8f7ca8f63fc76511c70bc23
Tushar Bhardwaj <116137083+TuShArBhArDwA@users.noreply.github.com>
2025-01-01 13:22:30 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Tushar Bhardwaj
+          Copyright © 2025 Tushar Bhardwaj
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
