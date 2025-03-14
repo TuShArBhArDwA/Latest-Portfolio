a941221d31a0981ab3cd2a9dbecd5b3a1e729c5f
Tushar Bhardwaj <116137083+TuShArBhArDwA@users.noreply.github.com>
2025-03-14 10:24:35 +0530

    Update CodingProfiles.tsx

--- components/CodingProfiles.tsx
+++ components/CodingProfiles.tsx
@@ -7,14 +7,14 @@ const codingProfiles = [
     name: "LeetCode",
     url: "https://leetcode.com/u/Tusharr2004/",
     logo: "/leetcode-logo.svg",
-    description: "Solved over 870 questions with a streak of 490 days.",
+    description: "Solved over 880 questions with a streak of 500 days.",
   },
   {
     id: 2,
     name: "GeeksforGeeks",
     url: "https://www.geeksforgeeks.org/user/tusharbhard2004/",
     logo: "/gfg-logo.svg",
-    description: "Institute Rank 80, solved 530 questions with a streak of 325 days.",
+    description: "Institute Rank 75, solved 540 questions with a streak of 325 days.",
   },
   {
     id: 3,
@@ -22,14 +22,14 @@ const codingProfiles = [
     url: "https://www.codechef.com/users/tushar2004b",
     logo: "/codelogo.svg",
     // logo: "/codechef.jfif",
-    description: "Division 3 with a streak of 695 days.",
+    description: "Division 3 with a streak of 705 days.",
   },
   {
     id: 4,
     name: "Coding Ninjas",
     url: "https://www.naukri.com/code360/profile/Bhar_dwajTushar",
     logo: "/cnlogo.svg",
-    description: "325-day streak, solved over 720 MCQs and 70 coding problems.",
+    description: "335-day streak, solved over 720 MCQs and 70 coding problems.",
   },
 ];
 
